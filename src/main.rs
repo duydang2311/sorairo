e057a9c0113b6f7a5c019318c01f6351c81c5ed2
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2026-03-04 02:39:46 +0700

    feat: add resume, pause, seek, progress bar; improve player state

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod player;
 
+use std::time::Duration;
+
+use eframe::egui::{
+    self, Color32, CornerRadius, ProgressBar, Rect, Sense, Shadow, Stroke, style::HandleShape, vec2,
+};
 use player::{PlayerState, SorairoPlayer};
-use eframe::egui::{self};
 
 #[derive(Default)]
 struct Sorairo {
@@ -11,24 +15,89 @@ struct Sorairo {
 
 impl eframe::App for Sorairo {
     fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::MenuBar::new().ui(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Open").clicked()
+                        && let Some(path) = rfd::FileDialog::new().pick_file()
+                    {
+                        let path = path.display().to_string();
+                        self.player.clear();
+                        self.player.play(&path);
+                        self.file_path = Some(path);
+                    }
+                    if ui.button("Quit").clicked() {
+                        ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
+                    }
+                });
+            })
+        });
         egui::CentralPanel::default().show(ctx, |ui| {
-            if ui.button("Select file").clicked()
-                && let Some(path) = rfd::FileDialog::new().pick_file()
-            {
-                let path = path.display().to_string();
-                self.player.clear();
-                self.player.play(&path);
-                self.file_path = Some(path);
-            }
-
             if let Some(path) = &self.file_path {
                 ui.label(path);
             }
+            if let PlayerState::Loaded(loaded) = self.player.get_state() {
+                if !loaded.player.is_paused() {
+                    ctx.request_repaint_after(Duration::from_millis(33));
+                }
 
-            if let PlayerState::Playing(_) = self.player.get_state() {
-                ui.label(format!("time: {}", self.player.get_elapsed_secs()));
-                ui.label(format!("volume = {}", self.player.get_volume()));
-                ctx.request_repaint_after(std::time::Duration::from_millis(100));
+                ui.horizontal(|ui| {
+                    let elapsed = self.player.get_elapsed_secs() as u64;
+                    let total = self.player.get_total_secs() as u64;
+                    ui.label(format!("{:02}:{:02}", elapsed / 60, elapsed % 60));
+                    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                        ui.label(format!("{:02}:{:02}", total / 60, total % 60));
+                    });
+                });
+
+                let (rect, resp) = ui
+                    .allocate_exact_size(vec2(ui.available_width(), 8.0), Sense::click_and_drag());
+                let padding = 2.0;
+                let active_rect = rect.shrink(padding);
+                let progress =
+                    (self.player.get_elapsed_secs() / self.player.get_total_secs()) as f32;
+                let progress_width = active_rect.width() * progress;
+                let progress_rect = Rect::from_min_max(
+                    active_rect.min,
+                    egui::pos2(active_rect.left() + progress_width, active_rect.bottom()),
+                );
+
+                ui.painter()
+                    .rect_filled(rect, 2.0, ui.visuals().widgets.inactive.bg_fill);
+                if resp.hovered() {
+                    ui.painter().rect_filled(
+                        progress_rect,
+                        2.0,
+                        ui.visuals().widgets.hovered.fg_stroke.color,
+                    );
+                } else {
+                    ui.painter().rect_filled(
+                        progress_rect,
+                        2.0,
+                        ui.visuals().widgets.inactive.fg_stroke.color,
+                    );
+                }
+                if resp.dragged() || resp.clicked() {
+                    if let Some(pointer_pos) = resp.interact_pointer_pos() {
+                        let percent = ((pointer_pos.x - active_rect.left()) / active_rect.width())
+                            .clamp(0.0, 1.0);
+                        let new_time = percent as f64 * self.player.get_total_secs();
+                        self.player.seek(new_time);
+                    }
+                }
+                if ui
+                    .button(match loaded.player.is_paused() {
+                        true => "Resume",
+                        false => "Pause",
+                    })
+                    .clicked()
+                {
+                    match loaded.player.is_paused() {
+                        true => self.player.resume(),
+                        false => self.player.pause(),
+                    };
+                }
+
             }
         });
     }
@@ -39,7 +108,9 @@ impl Sorairo {}
 fn main() -> eframe::Result {
     env_logger::init();
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([640.0, 480.0])
+            .with_min_inner_size([640.0, 480.0]),
         ..Default::default()
     };
     eframe::run_native(
@@ -56,32 +127,61 @@ fn main() -> eframe::Result {
 }
 
 fn replace_fonts(ctx: &egui::Context) {
-    // Start with the default fonts (we will be adding to them rather than replacing them).
     let mut fonts = egui::FontDefinitions::default();
-
-    // Install my own font (maybe supporting non-latin characters).
-    // .ttf and .otf files supported.
     fonts.font_data.insert(
         "segoeui".to_owned(),
         std::sync::Arc::new(egui::FontData::from_static(include_bytes!(
             "C:\\Windows\\Fonts\\segoeui.ttf"
         ))),
     );
-
-    // Put my font first (highest priority) for proportional text:
     fonts
         .families
         .entry(egui::FontFamily::Proportional)
         .or_default()
         .insert(0, "segoeui".to_owned());
-
-    // Put my font as last fallback for monospace:
     fonts
         .families
         .entry(egui::FontFamily::Monospace)
         .or_default()
         .push("segoeui".to_owned());
-
-    // Tell egui to use these fonts:
     ctx.set_fonts(fonts);
+    ctx.all_styles_mut(|style| {
+        style.visuals.menu_corner_radius = CornerRadius::ZERO;
+        style.visuals.window_corner_radius = CornerRadius::ZERO;
+        style.visuals.widgets.hovered.corner_radius = CornerRadius::ZERO;
+        style.visuals.widgets.active.corner_radius = CornerRadius::ZERO;
+        style.visuals.widgets.open.corner_radius = CornerRadius::ZERO;
+        style.visuals.widgets.inactive.corner_radius = CornerRadius::ZERO;
+        style.visuals.widgets.noninteractive.corner_radius = CornerRadius::ZERO;
+        style.visuals.popup_shadow = Shadow {
+            offset: [1, 1],
+            blur: 8,
+            spread: 0,
+            color: Color32::from_hex("#00000011").expect("hex must be valid"),
+        }
+    });
+    ctx.style_mut_of(egui::Theme::Light, |style| {
+        style.visuals.window_fill = Color32::from_hex("#f3f3f3").expect("hex must be valid");
+        style.visuals.panel_fill = Color32::from_hex("#f3f3f3").expect("hex must be valid");
+        style.visuals.window_stroke = Stroke::new(
+            1.0,
+            Color32::from_hex("#c5c5c5").expect("hex must be valid"),
+        );
+        style.visuals.widgets.noninteractive.bg_stroke = Stroke::new(
+            1.0,
+            Color32::from_hex("#d5d5d5").expect("hex must be valid"),
+        );
+    });
+    ctx.style_mut_of(egui::Theme::Dark, |style| {
+        style.visuals.window_fill = Color32::from_hex("#202020").expect("hex must be valid");
+        style.visuals.panel_fill = Color32::from_hex("#202020").expect("hex must be valid");
+        style.visuals.window_stroke = Stroke::new(
+            1.0,
+            Color32::from_hex("#424242").expect("hex must be valid"),
+        );
+        style.visuals.widgets.noninteractive.bg_stroke = Stroke::new(
+            1.0,
+            Color32::from_hex("#323232").expect("hex must be valid"),
+        );
+    });
 }
--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,6 @@
 use std::{
     fs::File,
+    io::{Read, Seek},
     num::NonZero,
     sync::{
         Arc,
@@ -8,7 +9,9 @@ use std::{
     time::Duration,
 };
 
-use rodio::{Decoder, MixerDeviceSink, Player, SampleRate, Source};
+use rodio::{
+    ChannelCount, Decoder, MixerDeviceSink, Player, SampleRate, Source, source::SeekError,
+};
 
 #[derive(Default)]
 pub struct SorairoPlayer {
@@ -17,21 +20,16 @@ pub struct SorairoPlayer {
 
 pub enum PlayerState {
     Idle,
-    Ready(ReadyState),
-    Playing(PlayingState),
+    Loaded(LoadedState),
 }
 
-pub struct ReadyState {
-    sink: MixerDeviceSink,
-    player: Player,
-}
-
-pub struct PlayingState {
-    sink: MixerDeviceSink,
-    player: Player,
-    sample_rate: SampleRate,
-    channels: NonZero<u16>,
-    progress: Arc<AtomicU64>,
+pub struct LoadedState {
+    pub sink: MixerDeviceSink,
+    pub player: Player,
+    pub sample_rate: SampleRate,
+    pub channels: NonZero<u16>,
+    pub progress: Arc<AtomicU64>,
+    pub total_duration: Duration,
 }
 
 struct ProgressTracked<I> {
@@ -44,25 +42,9 @@ impl<I: Iterator> Iterator for ProgressTracked<I> {
     fn next(&mut self) -> Option<Self::Item> {
         let n = self.inner.next();
         if n.is_some() {
-            // Every time a sample is played, increment the counter
             self.counter.fetch_add(1, Ordering::Relaxed);
         }
         n
-    }
-}
-
-impl<I: Source> Source for ProgressTracked<I> {
-    fn current_span_len(&self) -> Option<usize> {
-        self.inner.current_span_len()
-    }
-    fn channels(&self) -> NonZero<u16> {
-        self.inner.channels()
-    }
-    fn sample_rate(&self) -> SampleRate {
-        self.inner.sample_rate()
-    }
-    fn total_duration(&self) -> Option<Duration> {
-        self.inner.total_duration()
     }
 }
 
@@ -72,12 +54,37 @@ impl Default for PlayerState {
     }
 }
 
-impl From<PlayingState> for ReadyState {
-    fn from(playing: PlayingState) -> Self {
-        ReadyState {
-            sink: playing.sink,
-            player: playing.player,
-        }
+impl<R> Source for ProgressTracked<Decoder<R>>
+where
+    R: Read + Seek,
+{
+    #[inline]
+    fn current_span_len(&self) -> Option<usize> {
+        self.inner.current_span_len()
+    }
+
+    #[inline]
+    fn channels(&self) -> ChannelCount {
+        self.inner.channels()
+    }
+
+    fn sample_rate(&self) -> SampleRate {
+        self.inner.sample_rate()
+    }
+
+    #[inline]
+    fn total_duration(&self) -> Option<Duration> {
+        self.inner.total_duration()
+    }
+
+    // #[inline]
+    fn try_seek(&mut self, pos: Duration) -> Result<(), SeekError> {
+        self.inner.try_seek(pos)?;
+        let sample_rate = u32::from(self.sample_rate()) as f64;
+        let channels = u16::from(self.inner.channels()) as f64;
+        let new_sample_pos = (pos.as_secs_f64() * sample_rate * channels) as u64;
+        self.counter.store(new_sample_pos, Ordering::Relaxed);
+        Ok(())
     }
 }
 
@@ -87,81 +94,104 @@ impl SorairoPlayer {
     }
 
     pub fn play(&mut self, path: &str) {
-        let ready = match std::mem::replace(&mut self.state, PlayerState::Idle) {
+        let file = File::open(path).unwrap();
+        let source = Decoder::try_from(file).unwrap();
+        let progress = Arc::new(AtomicU64::new(0));
+        let loaded = match std::mem::replace(&mut self.state, PlayerState::Idle) {
             PlayerState::Idle => {
                 let sink = rodio::DeviceSinkBuilder::open_default_sink()
                     .expect("open default audio stream");
                 let player = rodio::Player::connect_new(sink.mixer());
-                ReadyState { sink, player }
-            }
-            PlayerState::Ready(ready) => ready,
-            PlayerState::Playing(playing) => {
-                playing.player.clear();
-                ReadyState {
-                    sink: playing.sink,
-                    player: playing.player,
+                LoadedState {
+                    sink,
+                    player,
+                    sample_rate: source.sample_rate(),
+                    channels: source.channels(),
+                    progress: progress.clone(),
+                    total_duration: source
+                        .total_duration()
+                        .expect("source must have total_duration"),
                 }
             }
-        };
-        let file = File::open(path).unwrap();
-        let source = Decoder::try_from(file).unwrap();
-
-        let progress = Arc::new(AtomicU64::new(0));
-
-        let playing = PlayingState {
-            sample_rate: source.sample_rate(),
-            channels: source.channels(),
-            progress: progress.clone(),
-            sink: ready.sink,
-            player: ready.player,
+            PlayerState::Loaded(mut loaded) => {
+                loaded.sample_rate = source.sample_rate();
+                loaded.channels = source.channels();
+                loaded.progress = progress.clone();
+                loaded.total_duration = source
+                    .total_duration()
+                    .expect("source must have total_duration");
+                loaded
+            }
         };
 
-        playing.player.append(ProgressTracked {
+        loaded.player.append(ProgressTracked {
             inner: source,
             counter: progress,
         });
-        playing.player.play();
-        // playing.sink.mixer().add(ProgressTracked {
-        //     inner: source,
-        //     counter: progress,
-        // });
-
-        self.state = PlayerState::Playing(playing);
+        loaded.player.play();
+        self.state = PlayerState::Loaded(loaded);
     }
 
     pub fn clear(&mut self) {
-        self.state = match std::mem::replace(&mut self.state, PlayerState::Idle) {
-            PlayerState::Idle => PlayerState::Idle,
-
-            PlayerState::Ready(ready) => {
-                ready.player.clear();
-                PlayerState::Ready(ready)
-            }
-
-            PlayerState::Playing(playing) => {
-                playing.player.clear();
-
-                PlayerState::Ready(playing.into())
+        match &self.state {
+            PlayerState::Idle => {}
+            PlayerState::Loaded(loaded) => {
+                loaded.player.clear();
             }
         };
     }
 
     pub fn get_elapsed_secs(&self) -> f64 {
         match &self.state {
-            PlayerState::Playing(playing) => {
-                let samples = playing.progress.load(std::sync::atomic::Ordering::Relaxed);
-                samples as f64
-                    / (u32::from(playing.sample_rate) as f64 * u16::from(playing.channels) as f64)
+            PlayerState::Idle => 0.0,
+            PlayerState::Loaded(loaded) => {
+                let played_samples = loaded.progress.load(Ordering::Relaxed);
+                let elapsed = played_samples as f64
+                    / (u32::from(loaded.sample_rate) as f64 * u16::from(loaded.channels) as f64);
+                elapsed.min(loaded.total_duration.as_secs_f64())
             }
-            _ => 0.0,
+        }
+    }
+
+    pub fn get_total_secs(&self) -> f64 {
+        match &self.state {
+            PlayerState::Idle => 0.0,
+            PlayerState::Loaded(loaded) => loaded.total_duration.as_secs_f64(),
         }
     }
 
     pub fn get_volume(&self) -> f32 {
         match &self.state {
-            PlayerState::Ready(state) => state.player.volume(),
-            PlayerState::Playing(state) => state.player.volume(),
             PlayerState::Idle => 0.0,
+            PlayerState::Loaded(loaded) => loaded.player.volume(),
+        }
+    }
+
+    pub fn resume(&self) {
+        match &self.state {
+            PlayerState::Idle => {}
+            PlayerState::Loaded(loaded) => loaded.player.play(),
+        }
+    }
+
+    pub fn pause(&self) {
+        match &self.state {
+            PlayerState::Idle => {}
+            PlayerState::Loaded(loaded) => loaded.player.pause(),
+        }
+    }
+
+    pub fn seek(&self, secs: f64) {
+        match &self.state {
+            PlayerState::Idle => {}
+            PlayerState::Loaded(loaded) => {
+                _ = loaded
+                    .player
+                    .try_seek(Duration::from_secs_f64(secs))
+                    .inspect_err(|e| {
+                        eprintln!("{}", e);
+                    });
+            }
         }
     }
 }
