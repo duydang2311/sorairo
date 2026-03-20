377d7769f9d8e5e3de3914a4cd0db196d4e13467
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2026-03-20 23:45:44 +0700

    feat: dark theme

--- Sorairo/Common/UI/LightColors.cs
+++ Sorairo/Common/UI/LightColors.cs
@@ -4,14 +4,6 @@ namespace Sorairo.Common.UI;
 
 public static class LightColors
 {
-    // public static readonly Color Surface = Color.Parse("#fbfffa");
-    // public static readonly Color SurfaceSubtle = Color.Parse("#f6faf5");
-    // public static readonly Color SurfaceEmph = Color.Parse("#f2f6f1");
-    // public static readonly Color SurfaceBorder = Color.Parse("#d2dad0");
-
-    // public static readonly Color Primary = Color.Parse("#bddba7");
-    // public static readonly Color PrimaryFg = Color.Parse("#567939");
-
     public static readonly Color Surface = Color.Parse("#fefeff");
     public static readonly Color SurfaceSubtle = Color.Parse("#f6f9fb");
     public static readonly Color SurfaceEmph = Color.Parse("#f0f6f9");
@@ -31,4 +23,27 @@ public static class LightColors
     public static readonly Color PrimarySubtle = Color.Parse("#92cfee");
     public static readonly Color PrimaryEmph = Color.Parse("#88c5e4");
     public static readonly Color PrimaryFg = Color.Parse("#01699b");
+}
+
+public static class DarkColors
+{
+    public static readonly Color Surface = Color.Parse("#202021");
+    public static readonly Color SurfaceSubtle = Color.Parse("#252526");
+    public static readonly Color SurfaceEmph = Color.Parse("#2a2a2b");
+    public static readonly Color SurfaceBorder = Color.Parse("#2f2f30");
+
+    public static readonly Color Fg = Color.Parse("#ffffff");
+    public static readonly Color FgSubtle = Color.Parse("#c2c2c2");
+    public static readonly Color FgEmph = Color.Parse("#ffffff");
+    public static readonly Color FgMuted = Color.Parse("#aaaaaa");
+
+    public static readonly Color Base = Color.Parse("#2e2f30");
+    public static readonly Color BaseSubtle = Color.Parse("#323436");
+    public static readonly Color BaseEmph = Color.Parse("#373a3b");
+    public static readonly Color BaseBorder = Color.Parse("#3c3f40");
+
+    public static readonly Color Primary = Color.Parse("#1f3645");
+    public static readonly Color PrimarySubtle = Color.Parse("#163c54");
+    public static readonly Color PrimaryEmph = Color.Parse("#1c4159");
+    public static readonly Color PrimaryFg = Color.Parse("#01a8f9");
 }
